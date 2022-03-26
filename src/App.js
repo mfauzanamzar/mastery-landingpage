2f4e892ea147d7e5d0ffa34d54e1d1f3bcfa49ad
mfauzanamzar <mfauzanamzar@gmail.com>
2022-03-27 03:00:31 +0800

    remake

--- src/App.js
+++ src/App.js
@@ -6,22 +6,7 @@ import SliderTwo from './components/SliderTwo';
 import SliderThree from './components/SliderThree';
 import Footer from './components/Footer';
 
-
-const loadScript = function(src) {
-  var tag = document.createElement("script");
-  tag.async = false;
-  tag.src = src;
-  document.getElementsByTagName("body")[0].appendChild(tag);
-};
-
 function App() {
-
-  useEffect(() => {
-    loadScript('js/jquery.js');
-    loadScript('js/plugins.js');
-    loadScript('js/functions.js');
-
-  },[])
   return (
     <div className="App">
       <AppBar/>
